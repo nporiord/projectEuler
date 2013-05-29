7d6c3af2a41a1a455f594126cd3bb43c54b95e42
nporiord <nporiord@gmail.com>
2013-05-30 09:03:42 +1000

    Initial commit local

---
+++ src/main/Euler.hs
@@ -0,0 +1,49 @@
+module Euler 
+( p1
+  , p2
+  , p3
+  , p4
+  , p4_isPalindrome
+) where
+
+import Data.List
+ 
+p4 :: Int -> [Int]
+p4 rangeUpper = nub [x * y | x <- [rangeUpper,rangeUpper-1..100], y <- [rangeUpper,rangeUpper-1..100], p4_isPalindrome (x * y)]
+
+p4_isPalindrome :: Int -> Bool
+p4_isPalindrome number = reverse (show number) == show number
+
+-- Prime numbers etc.
+p3 :: Int -> [Int]
+p3 limit = p3_primeFactors limit
+
+p3_primeFactors :: Int -> [Int]
+p3_primeFactors limit = filter p3_isPrime ([1,2,3,4,5,limit])
+
+p3_isPrime :: Int -> Bool
+p3_isPrime factor = True
+
+--Question 2
+p2 :: Int -> Int
+p2 limit = sumFold (filter (\x -> even x) (p2_fib limit [1,1]))
+
+--Gives you back n Fib numbers.
+p2_fib :: Int -> [Int] -> [Int] 
+p2_fib m (x:y:xs) 
+  | x > m = y:xs
+  | otherwise = p2_fib m (x+y:x:y:xs)
+
+
+-- sum of all numbers < limit that are divisible by 3 and 5
+-- 10 -> 3 5 6 9 = 23 
+--Question 1
+p1 :: Int -> Int
+p1 limit = sumFold $ p1_1 limit
+
+p1_1 :: Int -> [Int]
+p1_1 limit = ( filter (\x -> x `mod` 3 == 0 || x `mod` 5 == 0) [1..limit-1])
+
+sumFold :: [Int] -> Int
+sumFold = foldl (+) 0 
+
---
+++ src/main/Main.hs
@@ -0,0 +1,52 @@
+module Main
+( main
+) where 
+
+import Test.HUnit
+import Euler
+
+-- Question 3
+test4_1 = TestCase ( assertEqual "4_1" [10201] (Euler.p4 102))
+test4_1 = TestCase ( assertEqual "4_2" [10201] (Euler.p4 102))
+
+
+-- Question 3
+test3_1 = TestCase ( assertEqual "3_1" [1] (Euler.p3 10))
+
+-- Question 2
+test2_1 = TestCase ( assertEqual "2_1" 10 (Euler.p2 10))
+test2_2 = TestCase ( assertEqual "2_2" 44 (Euler.p2 100))
+test2_3 = TestCase ( assertEqual "2_1" 798 (Euler.p2 1000))
+test2_4 = TestCase ( assertEqual "2_1" 3382 (Euler.p2 10000))
+test2_5 = TestCase ( assertEqual "2_1" 60696 (Euler.p2 100000))
+test2_6 = TestCase ( assertEqual "2_1" 1089154 (Euler.p2 1000000))
+test2_7 = TestCase ( assertEqual "2_1" 4613732 (Euler.p2 4000000))
+
+-- Question 1
+test1_2 = TestCase ( assertEqual "1_2" 23 (Euler.p1 10))
+test1_3 = TestCase ( assertEqual "1_3" 78 (Euler.p1 20))
+test1_4 = TestCase ( assertEqual "1_4" 195 (Euler.p1 30))
+test1_1 = TestCase ( assertEqual "1_1" 2318 (Euler.p1 100))
+test1_5 = TestCase ( assertEqual "1_5" 233168 (Euler.p1 1000))
+
+
+tests = TestList [
+	  TestLabel "test1_1" test1_1 
+	, TestLabel "test1_2" test1_2
+	, TestLabel "test1_3" test1_3
+	, TestLabel "test1_3" test1_4
+	, TestLabel "test1_5" test1_5
+	, TestLabel "test2_1" test2_1
+	, TestLabel "test2_2" test2_2
+	, TestLabel "test2_3" test2_3
+	, TestLabel "test2_4" test2_4
+	, TestLabel "test2_5" test2_5
+	, TestLabel "test2_6" test2_6
+	, TestLabel "test2_7" test2_7
+	, TestLabel "test3_1" test3_1
+	, TestLabel "test4_1" test4_1
+	, TestLabel "test4_2" test4_2
+	]
+
+main :: IO Counts
+main = runTestTT tests
